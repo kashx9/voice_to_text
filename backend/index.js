ef573f7e3bff3e056af50c7a39d3a0672196fde9
kashx9 <kashyapayush985@gmail.com>
2026-07-13 16:59:50 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,64 @@
+import express from 'express'
+import 'dotenv/config'
+import { WebSocketServer, WebSocket } from 'ws'
+
+const app = express()
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
+
+const DEEPGRAM_URL = 'wss://api.deepgram.com/v1/listen' +
+    '?model=nova-3' +
+    '&language=en' +
+    '&punctuate=true' +
+    '&interim_results=true'+
+    '&encoding=linear16' +
+    '&sample_rate=16000'
+
+const wss = new WebSocketServer({ server })
+
+wss.on('connection', (clientWs, req) => {//client connected
+    console.log(`[WS] Client connected`)
+
+    const dgWs = new WebSocket(DEEPGRAM_URL, {//create connection to deepgram using uel and api key
+        headers: { Authorization: `TOKEN ${process.env.DEEPGRAM_API_KEY}` }
+    })
+
+    dgWs.on('open', () => {//connected to deepgram
+        console.log(`[DG] Connected to Deepgram`)
+        if(clientWs.readyState === WebSocket.OPEN)
+            clientWs.send(JSON.stringify({type:'Ready'}))
+    })
+
+    dgWs.on('close', (code) => {
+        console.log(`[DG] Deepgram closed : ${code}`)
+        if (clientWs.readyState === WebSocket.OPEN) clientWs.close()
+    })
+
+    clientWs.on('message', (data) => {//when user or client is speaking send data to deepgram
+        // console.log('[WS] Received chunk from client, size:', data.length) 
+        if (dgWs.readyState === WebSocket.OPEN) dgWs.send(data)
+    })
+
+    dgWs.on('message', (data) => {//when deepgram is sending data send it to client
+        // console.log(`Raw DG data: ${data.toString()}`)
+        if (clientWs.readyState === WebSocket.OPEN) clientWs.send(data.toString())
+    })
+
+    dgWs.on('error', (err) => {
+        console.error('[DG] Error:', err.message)
+        if (clientWs.readyState === WebSocket.OPEN) {
+            clientWs.send(JSON.stringify({ type: 'Error', message: err.message }))
+        }
+    })
+
+    clientWs.on('close', () => {
+        console.log('[WS] Client disconnected')
+        if (dgWs.readyState === WebSocket.OPEN) dgWs.close()
+    })
+
+    clientWs.on('error',(err)=>{
+        console.error(`[WS] Error: ${err}`)
+    })
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,135 @@
+import { useRef, useState } from 'react'
+import './App.css'
+export default function App() {
+
+  const wsUrl = 'ws://localhost:3001'
+  const wsRef = useRef(null)
+  const [finalText, setFinalText] = useState("")
+  const [interimText, setInterimText] = useState("")
+
+  const streamRef = useRef(null)
+  const [isRecording, setIsRecording] = useState(false)
+  const [status, setStatus] = useState("Permission not granted yet")
+  const audioContextRef = useRef(null)
+  const sourceRef = useRef(null)
+  const processorRef = useRef(null)
+
+  async function permissionCheck() {
+    setFinalText('')
+    if (isRecording) return
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+      streamRef.current = stream
+
+      const ws = new WebSocket(wsUrl)
+      wsRef.current = ws
+
+      ws.onopen = () => {
+        console.log('Waiting to connect with Deepgram')
+      }
+
+      ws.onmessage = (e) => {
+        try {
+          console.log(`Raw data messages: ${e.data}`)
+          let data = JSON.parse(e.data)
+          console.log(data)
+
+          if (data.type === 'Ready') {
+            const audioContext = new AudioContext({ sampleRate: '16000' })
+            audioContextRef.current = audioContext
+
+            const source = audioContext.createMediaStreamSource(stream)
+            sourceRef.current = source
+
+            const processor = audioContext.createScriptProcessor(4096, 1, 1)
+            processorRef.current = processor
+
+            processor.onaudioprocess = (e) => {
+              const inputData = e.inputBuffer.getChannelData(0)
+              const pcm16Buffer = new Int16Array(inputData.length)
+              for (let i = 0; i < inputData.length; i++) {
+                const sample = Math.max(-1, Math.min(1, inputData[i]))
+                pcm16Buffer[i] = sample < 0 ? sample * 0x8000 : sample * 0x7fff
+              }
+              if (wsRef.current?.readyState === WebSocket.OPEN) {
+                wsRef.current.send(pcm16Buffer.buffer)
+              }
+            }
+
+            source.connect(processor)
+            processor.connect(audioContext.destination)
+            return
+          }
+          if (data.type === 'Results') {
+            const transcript = data.channel.alternatives[0].transcript || ''
+            console.log(transcript)
+            if (data.is_final) {
+              setFinalText((prev) => (prev ? prev + ' ' + transcript : transcript))
+              setInterimText('')
+            }
+            else setInterimText(transcript)
+          }
+        } catch (error) {
+          console.error(error)
+        }
+      }
+      setStatus("Permission granted! Recording...")
+      setIsRecording(true)
+    } catch (error) {
+      setStatus(`Permission denied: ${error}`)
+      setIsRecording(false)
+    }
+  }
+  function stopMic() {
+    if (streamRef.current) {
+      streamRef.current.getTracks().forEach(track => track.stop())
+      streamRef.current = null
+
+      // mediaRecorderRef.current.onstop = () => {
+      //   const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/webm' })
+      //   const formData = new FormData()
+      //   formData.append('file', audioBlob, 'recording.webm')
+      //   console.log(audioChunksRef.current)
+      // }
+
+      if (wsRef.current.readyState === WebSocket.OPEN) {
+        wsRef.current.close()
+        wsRef.current = null
+      }
+      if (processorRef.current) processorRef.current.disconnect()
+      if (audioContextRef.current) audioContextRef.current.close()
+
+      setStatus("Mic turned off")
+      setIsRecording(false)
+    }
+  }
+
+  return (
+    <div className='main'>
+      <div>
+        <h1>Voice to text converter</h1>
+        <p>Click on the button to start speaking</p>
+      </div>
+      <div>
+        <p className="transcription-box">
+          {finalText}<span style={{ opacity: 0.5 }}>{interimText}</span>
+        </p>
+      </div>
+      <div>
+        <button
+          style={isRecording ? { backgroundColor: "red" } : { backgroundColor: "white" }}
+          onClick={permissionCheck}
+          disabled={isRecording}
+        >
+          Start Recording
+        </button>
+        <button
+          onClick={stopMic}
+        >
+          Stop recording
+        </button>
+        <p>Status: {status}</p>
+      </div>
+    </div>
+  )
+}
